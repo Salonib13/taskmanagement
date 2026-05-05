dde46c9cbe8753567ef0e2b60bcce96572b0126b
saloni bhingare <salonibhingare005@gmail.com>
2026-05-05 22:12:46 +0530

    Add Task entity and verify database table creation

--- src/main/java/com/example/taskmanagement/TaskmanagementApplication.java
+++ src/main/java/com/example/taskmanagement/TaskmanagementApplication.java
@@ -1,4 +1,4 @@
-package task.com.example.taskmanagement;
+package com.example.taskmanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
